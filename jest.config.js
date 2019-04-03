3a822b62c2f3966211a076311adac262945cf93a
Gabriele <gabrielebonumore@gmail.com>
2019-04-03 15:53:57 +0200

    Jest + Enzyme Introduction

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+// jest.config.js
+module.exports = {
+  verbose: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/utility/*.{js,jsx}',
+    '!/**/*.test.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/*.{css,scss}',
+  ],
+  moduleNameMapper: {
+    '^Api(.*)$': '<rootDir>web/src/api$1',
+    '^Components(.*)$': '<rootDir>web/src/components$1',
+    '^Store(.*)$': '<rootDir>web/src/store$1',
+    '^Utility(.*)$': '<rootDir>web/src/utility$1',
+    '.*\\.(css|scss)$': '<rootDir>/internals/helpers/SCSSStub.js',
+  },
+  coverageThreshold: {
+    global: {
+      statements: 98,
+      branches: 91,
+      functions: 98,
+      lines: 98,
+    },
+  },
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  setupFilesAfterEnv: [
+    './internals/enzyme.setup.js',
+  ],
+};
