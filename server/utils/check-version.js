0824d304d5b1bc460e9c33c89585a06b50ef3b22
Bartolo Amico <bartolo.amico@gmail.com>
2019-05-07 16:44:29 +0200

    Fix
    Try to fix appveyor configuration

--- server/utils/check-version.js
+++ server/utils/check-version.js
@@ -1,5 +1,4 @@
-const chalk = require('chalk');
-const semver = require('semver');
+
 const childProcess = require('child_process');
 const packageConfig = require('../../package.json');
 
@@ -27,12 +26,12 @@ module.exports = () => {
   for (let i = 0; i < versionRequirements.length; i += 1) {
     const mod = versionRequirements[i];
     if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
-      warnings.push(`${mod.name} : ${chalk.red(mod.currentVersion)} should be ${chalk.green(mod.versionRequirement)}`);
+      warnings.push(`${mod.name} : ${mod.currentVersion} should be ${mod.versionRequirement}`);
     }
   }
 
   if (warnings.length) {
-    console.log(chalk.yellow('To use this template, you must update following to modules:')); // eslint-disable-line
+    console.log('To use this template, you must update following to modules:'); // eslint-disable-line
 
     for (let i = 0; i < warnings.length; i += 1) {
       const warning = warnings[i];
